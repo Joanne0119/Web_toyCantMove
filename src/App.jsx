eecefa34ac522f00ef4966c7e459b6f48ed35802
劉丞恩 <114967562+Joanne0119@users.noreply.github.com>
2025-10-20 11:35:13 +0800

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { GameProvider } from './context/GameContext';
 import EnterName from './pages/EnterName';
