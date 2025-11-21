850f6f8dd97daa8478e30ac774f65628974a7efe
劉丞恩 <114967562+Joanne0119@users.noreply.github.com>
2025-11-21 14:48:57 +0800

    add vconsole for debug

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
+import VConsole from 'vconsole';
+
+const urlParams = new URLSearchParams(window.location.search);
+if (urlParams.get('debug') === 'true' || process.env.NODE_ENV === 'development') {
+  new VConsole({ theme: 'dark' }); 
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
