5ea0b27c795a0d3c999b883a84ed1b93cbf93953
劉丞恩 <114967562+Joanne0119@users.noreply.github.com>
2026-05-07 00:10:09 +0800

    update foodlevel image

--- src/pages/ChooseLevel.jsx
+++ src/pages/ChooseLevel.jsx
@@ -7,7 +7,7 @@ import LazyImage from '@/components/LazyImage';
 const levels = [
   { name: "玩具紙箱", sceneName: "4_Toybox", image: "/images/toyboxLevel.png", inputType: "gyro", disable: false },
   { name: "塗鴉畫紙", sceneName: "4_ColorPaper", image: "/images/tableLevel.png", inputType: "gyro", disable: false },
-  { name: "瘋狂餐桌", sceneName: "4_TapEat", image: "/images/tableLevel.png", inputType: "tap", disable: false },
+  { name: "瘋狂餐桌", sceneName: "4_TapEat", image: "/images/foodLevel.png", inputType: "tap", disable: false },
   // { name: "凌亂床鋪", sceneName: "4_Bed", image: "/images/bedLevel.png", inputType: "gyro", disable: true }
 ];
 
