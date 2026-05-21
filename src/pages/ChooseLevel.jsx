5a55faf951ef7a577cd34dc38a397dd16d5f2191
dddCrazy87 <n126970@gmail.com>
2026-05-21 15:21:26 +0800

    update spy game

--- src/pages/ChooseLevel.jsx
+++ src/pages/ChooseLevel.jsx
@@ -8,7 +8,7 @@ const levels = [
   { name: "玩具紙箱", sceneName: "4_Toybox", image: "/images/toyboxLevel.png", inputType: "gyro", disable: false },
   { name: "塗鴉畫紙", sceneName: "4_ColorPaper", image: "/images/tableLevel.png", inputType: "gyro", disable: false },
   { name: "瘋狂餐桌", sceneName: "4_TapEat", image: "/images/foodLevel.png", inputType: "tap", disable: false },
-  { name: "瘋狂餐桌", sceneName: "4_SpyGame", image: "/images/foodLevel.png", inputType: "spy", disable: false },
+  { name: "數學考卷", sceneName: "4_SpyGame", image: "/images/foodLevel.png", inputType: "spy", disable: false },
 ];
 
 const CARD_WIDTH_PERCENT = 65; // 卡片佔容器寬度的百分比
